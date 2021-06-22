dbf1e1524fb029ce27c5e49fcc3de5c0481f763c
ariuka11 <you@example.com>
2021-06-22 09:14:15 -0400

    Fixed file

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './testimonial.scss'
+export default function Testimonials() {
+    return (
+        <div className = 'testimonials'>
+            
+        </div>
+    )
+}
