96eec89a25de51557ce5195a4688444ef71293b1
ariuka11 <you@example.com>
2021-06-22 09:53:18 -0400

    More components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Topbar from "./components/Topbar/Topbar";
+import Intro from './components/Intro/Intro'
+import Portfolio from "./components/Portfolio/Portfolio";
+import Projects from "./components/Projects/Projects";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Contact from "./components/Contact/Contact";
+import './'
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Projects />
+        <Testimonials />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
